af063fe9648d3b35dfd92f143aaefcdae2d28f18
GrahamSH <grahamshllk@gmail.com>
2024-08-10 17:33:24 -0400

    add swagger bc silly

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+
 import puppeteer from "puppeteer";
 const browser = await puppeteer.launch();
 const takeScreenshot = async (html: string, selector: string) => {
@@ -13,24 +15,29 @@ const takeScreenshot = async (html: string, selector: string) => {
   return buffer;
 };
 
-const app = new Elysia().get("/", () => "Hello Elysia");
-app.post(
-  "/img.png",
-  async ({ body: { html, selector } }) => {
-    
-    const buffer = await takeScreenshot(html, selector || "body > *:first-child");
-    return new Response(buffer, {
-      headers: {
-        "Content-Type": "image/png",
-    }});
-  },
-  {
-    body: t.Object({
-      html: t.String(),
-      selector: t.Optional(t.String()),
-    }),
-  }
-);
+const app = new Elysia()
+  .use(swagger({excludeStaticFile:false}))
+  .get("/", () => "Hello Elysia")
+  .post(
+    "/img.png",
+    async ({ body: { html, selector } }) => {
+      const buffer = await takeScreenshot(
+        html,
+        selector || "body > *:first-child"
+      );
+      return new Response(buffer, {
+        headers: {
+          "Content-Type": "image/png",
+        },
+      });
+    },
+    {
+      body: t.Object({
+        html: t.String(),
+        selector: t.Optional(t.String()),
+      }),
+    }
+  );
 app.listen(3000);
 
 console.log(

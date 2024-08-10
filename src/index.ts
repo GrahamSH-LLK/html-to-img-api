a0b72201cdd0ea78c22b43be60228d670e5ebb1d
GrahamSH <grahamshllk@gmail.com>
2024-08-10 17:07:54 -0400

    its an api what can i tell you

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import puppeteer from "puppeteer";
+const browser = await puppeteer.launch();
+const takeScreenshot = async (html: string, selector: string) => {
+  const page = await browser.newPage();
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
+  const element = await page.$(selector);
+  const buffer = await element?.scrqeenshot({
+    optimizeForSpeed: true,
+  });
+
+  return buffer;
+};
+
+const app = new Elysia().get("/", () => "Hello Elysia");
+app.post(
+  "/img.png",
+  async ({ body: { html, selector } }) => {
+    
+    const buffer = await takeScreenshot(html, selector || "body > *:first-child");
+    return new Response(buffer, {
+      headers: {
+        "Content-Type": "image/png",
+    }});
+  },
+  {
+    body: t.Object({
+      html: t.String(),
+      selector: t.Optional(t.String()),
+    }),
+  }
+);
+app.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );

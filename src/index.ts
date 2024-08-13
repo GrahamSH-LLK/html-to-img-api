ecde103681aa112b1b12338d93e068e5928a7c69
GrahamSH <grahamshllk@gmail.com>
2024-08-13 07:31:51 -0400

    maybe will work on server?

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { rateLimit } from "elysia-rate-limit";
 
 import puppeteer from "puppeteer";
 const browser = await puppeteer.launch();
-const takeScreenshot = async (html: string, selector: string) => {
+const takeScreenshotHtml = async (html: string, selector: string) => {
   const page = await browser.newPage();
 
   await page.setContent(html, { waitUntil: "domcontentloaded" });
@@ -15,6 +15,17 @@ const takeScreenshot = async (html: string, selector: string) => {
 
   return buffer;
 };
+const takeScreenshotUrl = async (url: string, selector: string) => {
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  const element = await page.$(selector);
+  const buffer = await element?.screenshot({
+    optimizeForSpeed: true,
+  });
+
+  return buffer;
+};
+
 const cloudflareGenerator = (req: Request, server: any) =>
   // get client ip via cloudflare header first
   req.headers.get("CF-Connecting-IP") ??
@@ -23,15 +34,15 @@ const cloudflareGenerator = (req: Request, server: any) =>
   "";
 
 const app = new Elysia()
-  .use(
-    rateLimit({ max: 10, duration: 60 * 1000, generator: cloudflareGenerator })
-  ) // 10 requests per minute
+  //.use(
+    //rateLimit({ max: 10, duration: 60 * 1000, generator: cloudflareGenerator })
+  //) // 10 requests per minute
   .use(swagger({ excludeStaticFile: false }))
   .get("/", () => "Hello Elysia")
   .post(
     "/img.png",
     async ({ body: { html, selector } }) => {
-      const buffer = await takeScreenshot(
+      const buffer = await takeScreenshotHtml(
         html,
         selector || "body > *:first-child"
       );
@@ -47,7 +58,24 @@ const app = new Elysia()
         selector: t.Optional(t.String()),
       }),
     }
-  );
+  )
+  .get("/today.png", async () => {
+    let url = `https://lunch.grahamsh.com/dates/special/${encodeURIComponent(
+      new Date().toLocaleString(undefined, {
+        day: "2-digit",
+        year: "numeric",
+        month: "2-digit",
+      })
+    )}`;
+    url = `https://lunch.grahamsh.com/dates/special/08%2F27%2F2024`;
+
+    const buffer = await takeScreenshotUrl(url, "#day-container");
+    return new Response(buffer, {
+      headers: {
+        "Content-Type": "image/png",
+      },
+    });
+  });
 app.listen(3000);
 
 console.log(
